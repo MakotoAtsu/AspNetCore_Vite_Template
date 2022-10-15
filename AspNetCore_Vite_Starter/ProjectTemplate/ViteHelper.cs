5ab37d36759de145e535724118addff0e04f1170
MakotoAtsu <chris03152008@gmail.com>
2022-10-15 22:40:57 +0800

    Update ViteHelper.cs for supporting both vite2.x and vite3.x

--- AspNetCore_Vite_Starter/ProjectTemplate/ViteHelper.cs
+++ AspNetCore_Vite_Starter/ProjectTemplate/ViteHelper.cs
@@ -8,7 +8,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.NetworkInformation;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,41 +15,43 @@ namespace ProjectTemplate
 {
     public static class ViteHelper
     {
-        // done message of 'npm run serve' command.
-        private static string DoneMessage { get; } = "Dev server running at:";
+        private static bool PlatformIsWindows => OperatingSystem.IsWindows();
+
+        private static ILogger ViteLogger;
+
 
         /// <summary>
         /// Adds Connection to Vite Hosted VueApplication
         /// configured per <seealso cref="SpaOptions"/> on the <paramref name="spa"/>.
         /// NOTE: (this will create devcert.pfx and vite.config.js in your Vue Application on first run)
         /// </summary>
-        /// <param name="spa"></param>
-        public static void UseViteDevelopmentServer(this ISpaBuilder spa, int? port = null)
+        /// <param name="port">Vite hosting port</param>
+        /// <param name="sourcePath">Vite app source path</param>
+        public static void UseViteDevelopmentServer(this ISpaBuilder spa, int? port = null, string sourcePath = null)
         {
+
+            // throw error if node.js not installed.
+            EnsureNodeJSAlreadyInstalled();
+
             // Default HostingPort
-            if (spa.Options.DevServerPort == 0)
-                spa.Options.DevServerPort = 3000;
+            if (!port.HasValue)
+                port = 3000;
 
-            if (port.HasValue)
-                spa.Options.DevServerPort = port.Value;
+            spa.Options.DevServerPort = port.Value;
 
-            if (string.IsNullOrWhiteSpace(spa.Options.SourcePath))
+            if (!string.IsNullOrWhiteSpace(sourcePath))
+                spa.Options.SourcePath = sourcePath;
+            else if (string.IsNullOrWhiteSpace(spa.Options.SourcePath))
                 throw new ArgumentNullException("ISpaBuilder.Options.SourcePath", "Must specific Spa Client App path");
 
+
             var devServerEndpoint = new Uri($"https://localhost:{spa.Options.DevServerPort}");
-            var loggerFactory = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
             var webHostEnvironment = spa.ApplicationBuilder.ApplicationServices.GetService<IWebHostEnvironment>();
-            var logger = loggerFactory.CreateLogger("Vue");
+            ViteLogger = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>()?.CreateLogger("Vite");
 
-            // Is this already running
-            bool IsRunning = IPGlobalProperties.GetIPGlobalProperties()
-                .GetActiveTcpListeners()
-                .Select(x => x.Port)
-                .Contains(spa.Options.DevServerPort);
-
-            if (!IsRunning)
+            // If port not in used , launch vite dev server
+            if (!CheckPortInUsed(spa.Options.DevServerPort))
             {
-                var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
                 // export dev cert
                 var spaFolder = Path.Combine(webHostEnvironment.ContentRootPath, spa.Options.SourcePath);
@@ -59,121 +60,29 @@ namespace ProjectTemplate
 
                 var viteConfigPath = GetViteConfigFile(spaFolder);
 
-                var tempPfx = Path.Combine(spaFolder, "devcert.pfx");
+                var devCert = Path.Combine(spaFolder, "devcert.pfx");
                 var serverOptionFile = Path.Combine(spaFolder, $"serverOption{new FileInfo(viteConfigPath).Extension}");
 
 
                 // Check dev pfx exist
-                if (!File.Exists(serverOptionFile) || !File.Exists(tempPfx))
+                if (!File.Exists(serverOptionFile) || !File.Exists(devCert))
                 {
-                    var pfxPassword = Guid.NewGuid().ToString("N");
-                    logger.LogInformation($"Exporting dotnet dev cert to {tempPfx} for Vite");
-                    logger.LogDebug($"Export password: {pfxPassword}");
-                    var certExport = new ProcessStartInfo
-                    {
-                        FileName = isWindows ? "cmd" : "dotnet",
-                        Arguments = $"{(isWindows ? "/c dotnet " : "")}dev-certs https -v -ep {tempPfx} -p {pfxPassword}",
-                        RedirectStandardError = true,
-                        RedirectStandardInput = true,
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                    };
-
-                    var exportProcess = Process.Start(certExport);
-                    exportProcess.WaitForExit();
-                    if (exportProcess.ExitCode == 0)
-                        logger.LogInformation(exportProcess.StandardOutput.ReadToEnd());
-                    else
-                        logger.LogError(exportProcess.StandardError.ReadToEnd());
+                    var pwd = CreateCertPfxKey(devCert);
 
                     // Create serverOption file
-                    File.WriteAllText(serverOptionFile, BuildServerOption(tempPfx, pfxPassword));
-                    logger.LogInformation($"Creating Vite config: {serverOptionFile}");
+                    File.WriteAllText(serverOptionFile, BuildServerOption(devCert, pwd));
+                    ViteLogger?.LogInformation($"Creating Vite config: {serverOptionFile}");
 
                     InjectionViteConfig(viteConfigPath, serverOptionFile);
                 }
 
-                // Check Node_Module exists
-                if (!Directory.Exists(Path.Combine(spa.Options.SourcePath, "node_modules")))
-                {
-                    logger.LogWarning($"node_modules not found , run npm install...");
-                    // Install node modules
-                    var ps = Process.Start(new ProcessStartInfo()
-                    {
-                        FileName = isWindows ? "cmd" : "npm",
-                        Arguments = $"{(isWindows ? "/c npm " : "")}install",
-                        WorkingDirectory = spa.Options.SourcePath,
-                        RedirectStandardError = true,
-                        RedirectStandardInput = true,
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                    });
 
-                    ps.WaitForExit();
-                    logger.LogWarning($"npm install done.");
-                }
+                EnsureNodeModuleAlreadyInstalled(spa.Options.SourcePath);
 
                 // launch Vite development server
-                var runningPort = port.HasValue ? $" -- --port {port.Value}" : string.Empty;
-                var processInfo = new ProcessStartInfo
-                {
-                    FileName = isWindows ? "cmd" : "npm",
-                    Arguments = $"{(isWindows ? "/c npm " : "")}run dev{runningPort}",
-                    WorkingDirectory = spa.Options.SourcePath,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                };
-                var process = Process.Start(processInfo);
-                var tcs = new TaskCompletionSource<int>();
-
-                _ = Task.Run(() =>
-                {
-                    try
-                    {
-                        string line;
-                        while ((line = process.StandardOutput.ReadLine()?.Trim()) != null)
-                        {
-                            if (!String.IsNullOrEmpty(line))
-                            {
-                                logger.LogInformation(line);
-                                if (!tcs.Task.IsCompleted && line.Contains(DoneMessage, StringComparison.OrdinalIgnoreCase))
-                                {
-                                    tcs.SetResult(1);
-                                }
-                            }
-                        }
-                    }
-                    catch (EndOfStreamException ex)
-                    {
-                        logger.LogError(ex.ToString());
-                        tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
-                    }
-                });
-
-                _ = Task.Run(() =>
-                {
-                    try
-                    {
-                        string line;
-                        while ((line = process.StandardError.ReadLine()?.Trim()) != null)
-                        {
-                            logger.LogError(line);
-                        }
-                    }
-                    catch (EndOfStreamException ex)
-                    {
-                        logger.LogError(ex.ToString());
-                        tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
-                    }
-                });
-
-                if (!tcs.Task.Wait(spa.Options.StartupTimeout))
-                {
-                    throw new TimeoutException();
-                }
+                RunDevServer(spa.Options.SourcePath, spa.Options.DevServerPort, spa.Options.StartupTimeout);
             }
+
             spa.UseProxyToSpaDevelopmentServer(devServerEndpoint);
         }
 
@@ -225,7 +134,7 @@ namespace ProjectTemplate
         private static string BuildServerOption(string certfile, string pass)
         {
             var sb = new StringBuilder();
-            sb.AppendLine("import { ServerOptions } from 'vite'");
+            sb.AppendLine();
             sb.AppendLine();
             sb.AppendLine("export default {");
             sb.AppendLine($"https: {{ pfx: '{Path.GetFileName(certfile)}', passphrase: '{pass}' }}");
@@ -233,6 +142,159 @@ namespace ProjectTemplate
             sb.AppendLine();
 
             return sb.ToString();
+        }
+
+        private static bool CheckPortInUsed(int port)
+            => IPGlobalProperties.GetIPGlobalProperties()
+                .GetActiveTcpListeners()
+                .Select(x => x.Port)
+                .Contains(port);
+
+        /// <summary>
+        /// if 'node_module' not exist than run 'npm install'
+        /// </summary>
+        private static void EnsureNodeModuleAlreadyInstalled(string sourcePath)
+        {
+            // Check Node_Module exists
+            if (!Directory.Exists(Path.Combine(sourcePath, "node_modules")))
+            {
+                ViteLogger?.LogWarning($"node_modules not found , run npm install...");
+
+                // Install node modules
+                var ps = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = PlatformIsWindows ? "cmd" : "npm",
+                    Arguments = $"{(PlatformIsWindows ? "/c npm " : "")}install",
+                    WorkingDirectory = sourcePath,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                });
+
+                ps.WaitForExit();
+                ViteLogger?.LogWarning($"npm install done.");
+            }
+        }
+
+        /// <summary>
+        /// Throw exception if 'node --version' catch error
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        private static void EnsureNodeJSAlreadyInstalled()
+        {
+            var ps = Process.Start(new ProcessStartInfo()
+            {
+                FileName = PlatformIsWindows ? "cmd" : "node",
+                Arguments = $"{(PlatformIsWindows ? "/c node " : "")}--version",
+                //WorkingDirectory = /*SourcePath*/,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            });
+
+            ps.WaitForExit();
+
+            if (ps.ExitCode == 0)
+                return;
+
+            throw new Exception("Node.js is required to build and run this project. To continue, please install Node.js from https://nodejs.org/, and then restart your command prompt or IDE.");
+
+        }
+
+        /// <summary>
+        /// Create pfx key and return password
+        /// </summary>
+        private static string CreateCertPfxKey(string fileName)
+        {
+            var pfxPassword = Guid.NewGuid().ToString("N");
+            ViteLogger?.LogInformation($"Exporting dotnet dev cert to {fileName} for Vite");
+            ViteLogger?.LogDebug($"Export password: {pfxPassword}");
+            var certExport = new ProcessStartInfo
+            {
+                FileName = PlatformIsWindows ? "cmd" : "dotnet",
+                Arguments = $"{(PlatformIsWindows ? "/c dotnet " : "")}dev-certs https -v -ep {fileName} -p {pfxPassword}",
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            };
+
+            var exportProcess = Process.Start(certExport);
+            exportProcess.WaitForExit();
+            if (exportProcess.ExitCode == 0)
+                ViteLogger?.LogInformation(exportProcess.StandardOutput.ReadToEnd());
+            else
+                ViteLogger?.LogError(exportProcess.StandardError.ReadToEnd());
+
+            return pfxPassword;
+        }
+
+        private static void RunDevServer(string sourcePath, int port, TimeSpan timeout)
+        {
+            var runningPort = $" -- --port {port}";
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = PlatformIsWindows ? "cmd" : "npm",
+                Arguments = $"{(PlatformIsWindows ? "/c npm " : "")}run dev{runningPort}",
+                WorkingDirectory = sourcePath,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            };
+            var process = Process.Start(processInfo);
+            var tcs = new TaskCompletionSource<int>();
+
+            _ = Task.Run(() =>
+            {
+                try
+                {
+                    string? line;
+                    while ((line = process?.StandardOutput.ReadLine()?.Trim()) != null)
+                    {
+                        // Wait for done message
+                        if (!String.IsNullOrEmpty(line))
+                        {
+                            ViteLogger?.LogInformation(line);
+                            if (!tcs.Task.IsCompleted && line.Contains("VITE", StringComparison.OrdinalIgnoreCase))
+                                if (line.Contains("ready in", StringComparison.OrdinalIgnoreCase) || // for VITE v3
+                                    line.Contains("Dev server running at:", StringComparison.OrdinalIgnoreCase)) // for VITE v2
+                                {
+                                    tcs.SetResult(1);
+                                }
+                        }
+                    }
+                }
+                catch (EndOfStreamException ex)
+                {
+                    ViteLogger?.LogError(ex.ToString());
+                    tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
+                }
+            });
+
+            _ = Task.Run(() =>
+            {
+                try
+                {
+                    string? line;
+                    while ((line = process?.StandardError.ReadLine()?.Trim()) != null)
+                    {
+                        ViteLogger?.LogError(line);
+                    }
+                }
+                catch (EndOfStreamException ex)
+                {
+                    ViteLogger?.LogError(ex.ToString());
+                    tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
+                }
+            });
+
+            if (!tcs.Task.Wait(timeout))
+            {
+                throw new TimeoutException();
+            }
         }
     }
 }
